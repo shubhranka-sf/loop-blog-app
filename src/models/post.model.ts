78f7a00647e453e6ad2b5b17fbe9d737b90c99fc
Shubhranka Varma <shubhranka.varma@sourcefuse.com>
2025-02-07 15:49:05 +0530

    added models

---
+++ src/models/post.model.ts
@@ -0,0 +1,44 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Post extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  content: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  author: string;
+
+  @property({
+    type: 'date',
+    required: false,
+  })
+  publishedAt: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Post>) {
+    super(data);
+  }
+}
+
+export interface PostRelations {
+  // describe navigational properties here
+}
+
+export type PostWithRelations = Post & PostRelations;
